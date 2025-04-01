7524a47b0dbb29c46619be0e6c892680d58d2568
mateusdebarba <145573351+mateusdebarba@users.noreply.github.com>
2025-04-01 08:45:15 -0300

    01/04

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     const aumentaFontBotao =document.getElementById('aumentar-fonte');
+    const diminuiFontBotao =document.getElementById('diminuir-fonte');
 
     let tamanhoAtualFonte = 1;
 
@@ -7,4 +8,12 @@ document.addEventListener('DOMContentLoaded', function(){
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize= `${tamanhoAtualFonte}rem`;
     }) 
+
+    diminuiFontBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize= `${tamanhoAtualFonte}rem`;
+
+
+
+    })
 })
