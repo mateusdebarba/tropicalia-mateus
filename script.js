fcb7743f61378cd8e8455f4cb2b8cc0adceefee5
mateusdebarba <145573351+mateusdebarba@users.noreply.github.com>
2025-04-15 08:27:07 -0300

    15/04

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(){
+
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+
+    botaoDeAcessibilidade.addEventListener('click',function(){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+    })
+    
+
+
+
     const aumentaFontBotao =document.getElementById('aumentar-fonte');
     const diminuiFontBotao =document.getElementById('diminuir-fonte');
 
@@ -7,10 +19,10 @@ document.addEventListener('DOMContentLoaded', function(){
     aumentaFontBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize= `${tamanhoAtualFonte}rem`;
-    }) 
+    }); 
 
     diminuiFontBotao.addEventListener('click', function(){
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize= `${tamanhoAtualFonte}rem`;
-    })
-})
+    });
+});
